114280b99a2ef4ac224c8270383b247b23c65834
Sahil Ayank <sahilayank91@gmail.com>
2019-03-17 00:57:27 +0530

    Fixed fetch issues and added image features

--- App.js
+++ App.js
@@ -7,37 +7,44 @@ import {
   Button,
   TextInput,
   View,
+  Image,
   
   Text,ActivityIndicator
 } from "react-native";
-const axios = require('axios')
+import axios from 'axios';
 
 export default class print extends Component {
+  state = {
+    isLoading: true,
+    dataSource: null,
+    title: 'The title of the movie',
+    movieList:[],
+    description:'The description of the movie'
+  }
   constructor(props) {
     super(props);
-    this.state = { 
-      isLoading:true,
-      dataSource:null,
-      data:''
-     }
+    
   }
 
 
 componentDidMount()
 {
-    axios.get("https://facebook.github.io/react-native/movies.json")
-    .then(function(response){
-      let res =JSON.stringify(response);
-      Alert.alert(res);
-     /* this.setState({data:res})*/
-    })
-/* .then((getResponse)=>{
-       */
-      .catch((error) => {
-        console.error(error);
-      });
 
-}
+
+      axios.get(`https://facebook.github.io/react-native/movies.json`)
+        .then((response) => {
+          // Alert.alert(res);
+          this.setState({
+            title: JSON.stringify(response.data.title),
+            description:JSON.stringify(response.data.description),
+            movieList:response.data.movies
+          });
+        }).catch((error) => {
+          console.error(error);
+        });
+      }
+
+
 
 
 
@@ -49,35 +56,47 @@ _onalert()
 
   render() {
     return (
-      <View>
-          <Text>{this.state.data}</Text>
+      <ScrollView>
+      <View style={styles.container}>
+
+          <Text style={{fontSize:25,color:'green', flex:1, flexDirection:'row',justifyContent:'center'}}>KEEP IT UP!!</Text>
+
+          <Text style={styles.heading}>{this.state.title}</Text>
+          <Image style={{flex:0.3, justifyContent:'center',alignItems:'center',alignSelf:'center'}} source={require('./src/assets/Images/happy.jpg')} style={{ flex: 0.3, height: 200, width: 210 }} />
+
+          <Text>{this.state.description}</Text>
+          <View style={styles.list}>
+          {
+            this.state.movieList.length!=0 && this.state.movieList.map((item)=>(
+              <View style={{flex:1,flexDirection:'row'}}>
+                 <Text  style={{flex:0.2}}>{item.id}</Text>
+                 <Text  style={{flex:0.5}}>{item.title}</Text>
+                 <Text  style={{flex:0.3}}>{item.releaseYear}</Text> 
+              </View>
+               
+            ))
+          } 
+          </View>
+
       </View>
+      </ScrollView>
     );
   }
-
-  }
-
-
-
- /* render(){
-
-    return(
-  { if(this.state.isLoading)
-  {
-      <ActivityIndicator />
-}else{}
- /* let User = this.state.dataSource.map((val, key) => {
-
-
-    <View key={key} >{val.title}</View>
-  });
-  return (<View>{User}</View>);
-  }*/
-
-  
-
-
+}
  
-
-
-
+const styles = StyleSheet.create({
+  container:{
+   flex:1,
+  },
+  heading:{
+    fontSize:20,  
+    color:'red',
+  },
+  description:{
+   
+    fontSize:10,
+  },
+  list:{
+   flex:0.6
+  }
+})
